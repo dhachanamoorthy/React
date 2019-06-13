a3cd42ceb377990837f1b75f121a299ff32ea76e
Dhachanamoorthy <45748943+dhachanamoorthy@users.noreply.github.com>
2019-06-13 22:52:45 +0530

    ver 0.1

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="head">
+      <h5>LOGIN PORTAL</h5>
+      <div className="form-box">
+      <label>username:</label>
+      <input type="text" placeholder="username"/>
+      <label>password:</label>
+      <input type="text" placeholder="password"/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
